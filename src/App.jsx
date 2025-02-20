9c51f081e39680ba8de2dbd16b8f1c72cf067eca
Raghu <raghuvir9923@gmail.com>
2025-02-21 02:08:29 +0530

    added animated headings on login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import AnimatedHeading from './components/AnimatedHeading'; // new component
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-container">
-      {/* Draggable region added at the top */}
-      <div className="drag-bar"></div>
-      <div className="background-section"></div>
+      <div className="drag-bar" />
+      <div className="background-section">
+        <AnimatedHeading />
+      </div>
       <div className="form-section">
         <LoginForm />
       </div>
